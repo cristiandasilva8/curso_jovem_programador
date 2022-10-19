b3485a92645c733a9d8546eeb96328c8161d45b6
Cristian DC Silva <cristiandasilva8@gmail.com>
2022-10-19 15:04:03 -0300

    implementado o desafio dos 7 erros

--- classes_objetos/construtor_destrutor.php
+++ classes_objetos/construtor_destrutor.php
@@ -1,1 +1,23 @@
-<div class="titulo">Construtor e Destrutor</div>
+<div class="titulo">Desafio dos 7 erros</div>
+
+<?php
+interface Template {
+    function metodo1();
+    public function metodo2($parametros);
+}
+
+abstract class ClasseAbstrata extends Template{
+    public function metodo3(){
+
+        echo "Estou funcionando";
+    }
+}
+
+class Classe implements ClasseAbstrata{
+    function __constructor($parametros){
+
+    }
+}
+
+$exemplo = Classe();
+$exemplo.metodo3();

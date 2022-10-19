c495e1d011276e0e0a6be517f9e8c64242e46f50
= <juhchamp@gmail.com>
2022-10-19 16:43:56 -0300

    Tentativa de resolução dos 7 erros haha.

--- classes_objetos/desafio_sete_erros.php
+++ classes_objetos/desafio_sete_erros.php
@@ -3,21 +3,35 @@
 <?php
 interface Template {
     function metodo1();
-    public function metodo2($parametros);
+    public function metodo2(array $parametros);
 }
 
-abstract class ClasseAbstrata extends Template{
-    public function metodo3(){
+abstract class ClasseAbstrata implements Template {
 
+    public function metodo1() {
+        echo 'Metodo 1 implementado<br>';
+    }
+
+    public function metodo2($parametros) {
+        echo 'Metodo 2 implementado com os parametros:';
+        foreach($parametros as $p) {
+            echo "<br>$p";
+        }
+        echo '<br>';
+    }
+
+    public function metodo3(){
         echo "Estou funcionando";
     }
 }
 
-class Classe implements ClasseAbstrata{
+class Classe extends ClasseAbstrata {
     function __constructor($parametros){
 
     }
 }
 
-$exemplo = Classe();
-$exemplo.metodo3();
+$exemplo = new Classe();
+$exemplo->metodo1();
+$exemplo->metodo2(array('teste' => 999, 'erro' => 7));
+$exemplo->metodo3();
